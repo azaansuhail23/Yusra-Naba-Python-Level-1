81199cbd9f5b834c260bf1501db4d000371923f4
Azaan Suhail <azaan.suhail@gmail.com>
2026-07-27 20:27:50 +0530

    Session 3

---
+++ Session3/P1_Assignment_Operator.py
@@ -0,0 +1,28 @@
+x=10
+x +=1
+print(x)
+
+y=10
+y -= 5
+print(y)
+
+z=10
+z-=9  # z = z-9  = 10-9=1
+print(z)
+
+m=11
+m -= 11
+print(m)
+
+p=10
+p *=10 #p=p*10=10*10=100
+print(p)
+
+n=10
+n /=5 # n=n/5=10/5
+print(n)
+
+
+l=10
+l %=3
+print(l)
