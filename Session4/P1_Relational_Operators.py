96f782eefa27a9d1d0fee21249c9d63649822530
Azaan Suhail <azaan.suhail@gmail.com>
2026-07-30 20:32:24 +0530

    Session4

---
+++ Session4/P1_Relational_Operators.py
@@ -0,0 +1,18 @@
+x=10
+y=10
+z=5
+
+print(x==y)
+print(x==z)
+print(x>z)
+print(x<z)
+
+print("----")
+
+print(x>=10)  #x>10 or x==10
+print(x>=5)  # x>5 or x==5
+print(x<=5)  # x<5 or x==5   10<5--> False or 10==5 False
+
+print(x!=5)
+print(x!=10)
+
---
+++ Session4/P2_Logical_Operators.py
@@ -0,0 +1,8 @@
+x=10
+y=10
+z=5
+
+print(x>5 and x>2)
+print(x<5 and x>2)
+
+print(not(x<5 or x>2))
